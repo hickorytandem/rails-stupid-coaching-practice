46fc3eec41d48cbb154e3eb6ce590c5581445d43
Michael Carter <hickorytandem@gmail.com>
2021-09-16 12:47:46 +0900

    made a few style changes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,18 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @question = params[:question]
+    if @question == "I am going to work!"
+      @coach_answer = "Great!"
+    elsif
+      @question.end_with?("?")
+      @coach_answer = "Silly question! Get dressed and go to work!"
+    else
+      @coach_answer = "I don't care! Get dressed and go to work!"
+    end
+
   end
 
+  def about
+  end
 end
